30d73af72490083f50592616c7b1089117731818
Ivan5 <converse_ivan@hotmail.com>
2019-03-27 17:23:16 -0600

    make a setup components

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <b-nav>
+      <b-nav-item disabled>
+        <b>Fancy Quiz App</b>
+      </b-nav-item>
+      <b-nav-item disabled>
+        <b>Counter: 4/10</b>
+      </b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
